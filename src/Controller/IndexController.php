e40c28cbd70e7be8331722fc3a4f2a94d72f2027
Evgeny Nakapyuk <nameewgeniy@ya.ru>
2023-03-05 00:14:09 +0300

    add skeleton

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\PostRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/')]
+class IndexController extends AbstractController
+{
+    #[Route('/', name: 'app_index', methods: ['GET'])]
+    public function index(PostRepository $postRepository): Response
+    {
+        return $this->render('index/index.html.twig', [
+            'posts' => $postRepository->findAll(),
+        ]);
+    }
+}
